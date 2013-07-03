fa21e27c891385a79951d3db82f0d8f4fc6d6f60
root <root@crunchbang.(none)>
2013-07-03 16:11:51 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gettingstarted::Application.config.secret_token = 'cf25736b3cfbf0d88a3b5054dc1580bc6d131310649cbcd716fa29b194469ad57f0b40103f6f92b269dce76e8fb698e4bc195a0abc8d9b64b3210d45e2c9298d'
